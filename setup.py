3019a9f2772677e2e91d163ce14b99fca35f8804
Panuthep <falcon_270394@hotmail.co.th>
2026-06-07 22:41:25 +0700

    update version to v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="api-model-proxy",
-    version="0.1.0",
+    version="0.1.1",
     description="A proxy layer for API-based language models",
     author="panuthept",
     packages=find_packages(where="src"),
